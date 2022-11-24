5c27e82ddddea636851ec6e9ab6beb948bcd5c20
Siddharth Gandhi <113658358+siddharthgandhi19@users.noreply.github.com>
2022-11-24 11:21:35 +0530

    Update Program.cs

--- Opps/Program.cs
+++ Opps/Program.cs
@@ -4,12 +4,29 @@ namespace OOPSPrograms
 {
     class Program
     {
+
         static string jsonFilePath = @"E:\Bridgelabz\Oops\Opps\InventoryManagement\Inventory.json";
+        static string InventoryDataPath = @"E:\Bridgelabz\Oops\Opps\InventoryDataManagement\InventoryData.json";
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the OOPS Programs");
-            InventoryDetailManagement inventory = new InventoryDetailManagement();
-            inventory.ReadJSONFile(jsonFilePath);
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Welcome to the OOPS Programs");
+                Console.WriteLine(" 1. Inventory Details Management\n 2. Inventory Data Management");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                        InventoryDetailManagement inventory = new InventoryDetailManagement();
+                        inventory.ReadJSONFile(jsonFilePath);
+                        break;
+                     default:
+                        flag = false;
+                        Console.WriteLine("Try with right input");
+                        break;
+                }
+            }            
         }
     }
 }
