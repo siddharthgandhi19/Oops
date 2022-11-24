5c8ef417cafedf9d09d9a4fee4d516402576aed3
siddharth gandhi <siddharthgandhi@outlook.com>
2022-11-24 11:55:08 +0530

    [Siddharth]Add: Add Data To Inventory

--- Opps/InventoryDataManagement/InventoryDataManage.cs
+++ Opps/InventoryDataManagement/InventoryDataManage.cs
@@ -10,24 +10,74 @@ namespace Opps.InventoryDataManagement
 {
     public class InventoryDataManage
     {
+        List<Inventory> RiceList;
+        List<Inventory> WheatList;
+        List<Inventory> PulsesList;
+        InventoryData inventories;
         public void ReadJSONFile(string file)
         {
-            var jsondata = File.ReadAllText(file);
-            InventoryData inventories = JsonConvert.DeserializeObject<InventoryData>(jsondata);
-            List<Inventory> RiceList = inventories.Rice;
-            Display(RiceList);
-            List<Inventory> WheatList = inventories.Wheat;
-            Display(WheatList);
-            List<Inventory> PulsesList = inventories.Pulses;
-            Display(PulsesList);
+            var jsonData = File.ReadAllText(file);
+            inventories = JsonConvert.DeserializeObject<InventoryData>(jsonData);
+            RiceList = inventories.Rice;
+            WheatList = inventories.Wheat;
+            PulsesList = inventories.Pulses;
 
         }
-        public void Display(List<Inventory> inventories)
+        public void Display()
+        {
+            Read(RiceList);
+            Read(WheatList);
+            Read(PulsesList);
+        }
+        public void Read(List<Inventory> inventories)
         {
             foreach (var data in inventories)
             {
-                Console.WriteLine("Inventory Name: " + data.Name + "\n" + "Weight: " + data.Weight + "\n" + "Price Per Kg: " + data.PricePerKg + "\n" + "Total Price: " + data.Weight * data.PricePerKg);
+                Console.WriteLine(data.Name + " " + data.Weight + " " + data.PricePerKg);
             }
+            Console.WriteLine();
+        }
+
+        public void AddInventoryData(string inventoryName)
+        {
+            Inventory inventory = new Inventory();
+            switch (inventoryName)
+            {
+                case "Rice":
+                    Console.Write("Enter the name : ");
+                    inventory.Name = Console.ReadLine();
+                    Console.Write("Enter the Weight : ");
+                    inventory.Weight = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter the Price");
+                    inventory.PricePerKg = Convert.ToDouble(Console.ReadLine());
+                    RiceList.Add(inventory);
+                    break;
+                case "Wheat":
+                    Console.Write("Enter the name : ");
+                    inventory.Name = Console.ReadLine();
+                    Console.Write("Enter the Weight : ");
+                    inventory.Weight = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter the Price");
+                    inventory.PricePerKg = Convert.ToDouble(Console.ReadLine());
+                    WheatList.Add(inventory);
+                    break;
+                case "pulse":
+                    Console.Write("Enter the name : ");
+                    inventory.Name = Console.ReadLine();
+                    Console.Write("Enter the Weight : ");
+                    inventory.Weight = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter the Price");
+                    inventory.PricePerKg = Convert.ToDouble(Console.ReadLine());
+                    PulsesList.Add(inventory);
+                    break;
+            }
+        }
+
+        public void WriteJsonFile(string file)
+        {
+            var json = JsonConvert.SerializeObject(inventories);
+            File.WriteAllText(file, json);
         }
     }
 }
+    
--- Opps/Program.cs
+++ Opps/Program.cs
@@ -10,11 +10,12 @@ namespace OOPSPrograms
         static string InventoryDataPath = @"E:\Bridgelabz\Oops\Opps\InventoryDataManagement\InventoryData.json";
         static void Main(string[] args)
         {
+            InventoryDataManage manage = new InventoryDataManage();
             bool flag = true;
             while (flag)
             {
                 Console.WriteLine("Welcome to the OOPS Programs");
-                Console.WriteLine(" 1. Inventory Details Management\n 2. Inventory Data Management");
+                Console.WriteLine(" 1. Inventory Details Management\n 2. Inventory Data Management\n 3. Display\n 4. Add Inventory");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch(option)
                 {
@@ -22,9 +23,18 @@ namespace OOPSPrograms
                         InventoryDetailManagement inventory = new InventoryDetailManagement();
                         inventory.ReadJSONFile(jsonFilePath);
                         break;
-                    case 2:
-                        InventoryDataManage manage = new InventoryDataManage();
+                    case 2:                        
                         manage.ReadJSONFile(InventoryDataPath);
+                        break;
+                    case 3:                        
+                        manage.ReadJSONFile(InventoryDataPath);
+                        Console.Write("Enter Inventory Name to add(Rice, Wheat, Pulse) : ");
+                        string addName = Console.ReadLine();
+                        manage.ReadJSONFile(InventoryDataPath);
+                        manage.AddInventoryData(addName);
+                        break;
+                    case 4:
+                        manage.Display();
                         break;
                     default:
                         flag = false;
